a556e849122aee1d221c57b3986e13548e53ca11
marihab <marihabade@gmail.com>
2024-03-03 10:43:44 +0100

    remove getData() from /page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,11 @@ import Image from 'next/image'
 import { Navbar } from './components/navbar'
 import  Hero  from './components/hero'
 import Newest from './components/Newest'
-import { client, urlFor } from './lib/sanity'
 
 
-export async function getData() {
-  const query = "*[_type =='heroImage'][0]['bgimage']"
-  const data = await client.fetch(query)
-  return urlFor(data).url()
-}
 
-export default async function Home() {
+
+export default function Home() {
   return (
     <div className='bg-white pb-6 sm:pb-8 lg:pb-12 relative'>
       <Hero/>
